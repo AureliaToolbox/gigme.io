bf6967eef2bfaf20f607eed7f8bc555befb39617
pwkad <patrickwaltersc21@yahoo.com>
2016-10-21 20:59:07 -0500

    cmon user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   # before_action :authenticate_user!
-  # before_action :add_user
+  before_action :add_user
 
-  # def add_user
-  #   p '-' * 80
-  #   p current_user
-  #   @user = current_user
-  # end
+  def add_user
+    p '-' * 80
+    p current_user
+    @user = current_user
+  end
 end
