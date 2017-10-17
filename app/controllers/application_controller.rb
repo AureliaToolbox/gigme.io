8562fc75283eada592d622184af45442ac541a00
pwkad <patrickwaltersc21@yahoo.com>
2017-10-16 23:29:57 -0500

    Add ability to disable csrf for dev server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :add_user
   before_action :preload_data
+  skip_before_action :verify_authenticity_token, unless: csrf_required?
 
   def add_user
     return if current_user.blank?
@@ -22,5 +23,9 @@ class ApplicationController < ActionController::Base
     @listing_types = ListingType.all
     @experience_levels = ExperienceLevel.all
     @companies = Company.all
+  end
+
+  def csrf_required?
+    ENV['DISABLE_CSRF'] == true
   end
 end
